d704c9a7856a49e26920371b01f73d78f7e16a8e
bagelo-rafal <programista@bagelo.cz>
2020-10-22 10:51:09 +0200

    Pierwsze logowanie - uzupełnienie danych

---
+++ resources/views/partials/forms/first-login.blade.php
@@ -0,0 +1,36 @@
+<form style="max-width: 400px;margin: auto;">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="form-group row">
+        <label for="parentBorn" class="col-md-12">Data urodzenia</label>
+          <div class="col-md-12">
+            <div class="input-group date datepicker" id="parentBorn">
+            <input type="text" class="form-control">
+            </div>
+          </div>
+        </div>
+    </div>
+      <div class="col-md-6">
+      <div class="form-group row">
+          <label for="parent-sex" class="col-md-12">Płeć</label>
+          <div class="col-md-12">
+            <select id="parent-sex" name="parent-sex" class="custom-select">
+              <option value="woman">Kobieta</option>
+              <option value="man">Mężczyzna</option>
+            </select>
+          </div>
+        </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="patient-pesel" class="col-md-12">Numer PESEL</label>
+    <div class="col-md-12">
+      <div class="input-group">
+        <input id="patient-pesel" name="patient-pesel" type="number" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <button name="submit" type="submit" class="btn btn-primary">Kontynuuj</button>
+  </div>
+</form>
