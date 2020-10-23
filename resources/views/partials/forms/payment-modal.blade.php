e210d6554930392f2f4b5798ba1ac23bac66e65c
Rafal <rafal@MBP-Rafal.eastwest.com.pl>
2020-10-23 23:16:21 +0200

    Dodanie informacji kogo dotyczy wizyta i powodu wizyty

--- resources/views/partials/forms/payment-modal.blade.php
+++ resources/views/partials/forms/payment-modal.blade.php
@@ -1,17 +1,32 @@
 <form id="payment-form">
   <div class="modal-body">
+    <h4 class="my-3">Podstawowe informacje</h4>
+    <div class="row">
+      <div class="col-md-12 mb-3">
+        <label for="patient-visit">Którego członka rodziny dotyczy wizyta?</label>
+        <select id="patient-visit" name="patient-visit" class="custom-select">
+          <option value="">Jan Kowalski (#PESEL)</option>
+          <option value="">Zuzia Kowalska (#PESEL)</option>
+        </select>
+      </div>
+      <div class="col-md-12">
+        <label for="visit-reason">Czego dotyczy wizyta? Opisz dokładnie swój przypadek</label>
+        <textarea class="form-control" id="visit-reason" name="visit-reason" rows="6" required></textarea>
+      </div>
+    </div>
+    <h4 class="my-3">Dane do płatności</h4>
     <div class="row">
       <div class="col-md-12 mb-3">
         <label>Numer karty:</label>
-        <input id="card-number" name="card-number" class="form-control" data-inputmask-alias="9999-9999-9999-9999">
+        <input id="card-number" name="card-number" class="form-control" required data-inputmask-alias="9999-9999-9999-9999">
       </div>
       <div class="col-md-6">
         <label>Data ważności:</label>
-        <input id="card-date" name="card-date" class="form-control" data-inputmask-alias="99/99">
+        <input id="card-date" name="card-date" class="form-control" required data-inputmask-alias="99/99">
       </div>
       <div class="col-md-6 mb-3">
         <label>Kod CVC:</label>
-        <input id="card-cvc" name="card-cvc" class="form-control" data-inputmask-alias="999">
+        <input id="card-cvc" name="card-cvc" class="form-control" required data-inputmask-alias="999">
       </div>
     </div>
     <div class="row">
@@ -33,7 +48,7 @@
       <div>
         <div class="">
           <label for="checkbox_0" class="">
-          <input name="consent1" required id="consent1" type="checkbox" checked="checked" class="" value="consent1">
+          <input name="consent1" required id="consent1" type="checkbox" class="" value="consent1">
           Zapoznałem/am się z <a target="_blank" href="https://domowypediatra.pl/polityka-prywatnosci/">polityką prywatności</a> oraz <a target="_blank" href="https://domowypediatra.pl/regulamin/">regulaminem</a> platformy DomowyPediatra.pl i wyrażam zgodę na przetwarzanie moich danych osobowych przez firmę MedPartner sp. z.o.o.</label>
         </div>
       </div>
