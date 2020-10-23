7cf61c183cfb60f2606aa7abec2d93b8846c68e3
Rafal <rafal@MBP-Rafal.eastwest.com.pl>
2020-10-24 01:56:22 +0200

    Dodanie tabeli z informacjami od pacjenta dot wizyty

---
+++ resources/views/partials/tables/visit-patient-info.blade.php
@@ -0,0 +1,36 @@
+<table cellpadding="0" cellspacing="0" border="0" class="table table-bordered" id="patientdetails" width="100%">
+  <tbody>
+    <tr>
+      <td>
+        Imię i nazwisko
+      </td>
+      <td>
+        Justyna Nowak
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Data urodzenia
+      </td>
+      <td>
+        12/12/2014
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Numer PESEL
+      </td>
+      <td>
+        84231277332
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Powód wizyty
+      </td>
+      <td>
+        Źle się czuję, kręci mi się w głowie, wymiotuję. Potrzebuję pomocy
+      </td>
+    </tr>
+  </tbody>
+</table>
