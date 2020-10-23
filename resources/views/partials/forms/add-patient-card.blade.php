e31cd7e734a508483243775404fcd47d2a2713ed
Rafal <rafal@MBP-Rafal.eastwest.com.pl>
2020-10-24 01:55:52 +0200

    Dodanie formularza dodawania informacji do karty pacjenta

---
+++ resources/views/partials/forms/add-patient-card.blade.php
@@ -0,0 +1,10 @@
+<form id="addPatientCard">
+  <div class="row">
+    <div class="col-md-12">
+      <label>Uzupełnij kartę pacjenta</label>
+      <textarea class="form-control" rows="7" id="patientCard" name="patientCard"></textarea>
+    </div>
+   </div>
+   <button type="submit" class="btn btn-primary btn-sm mt-3">Dodaj</button>
+
+</form>
