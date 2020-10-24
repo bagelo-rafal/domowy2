fb662d20177513c039286a7ab35a92e2f206aac1
Rafal <rafal@MBP-Rafal.eastwest.com.pl>
2020-10-24 02:12:46 +0200

    Tabela z danymi użytkownika

---
+++ resources/views/partials/tables/user-info.blade.php
@@ -0,0 +1,52 @@
+<table cellpadding="0" cellspacing="0" border="0" class="table table-bordered" id="userInfo" width="100%">
+  <tbody>
+    <tr>
+      <td>
+        Imię i nazwisko
+      </td>
+      <td>
+        Justyna Nowak
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Płeć
+      </td>
+      <td>
+        Kobieta
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Data urodzenia
+      </td>
+      <td>
+        12/12/2010
+      </td>
+    </tr>
+    <tr>
+      <td>
+        PESEL
+      </td>
+      <td>
+        54332366554
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Adres e-mail
+      </td>
+      <td>
+        justyna@nowak.pl
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Numer telefonu
+      </td>
+      <td>
+        543223123
+      </td>
+    </tr>
+  </tbody>
+</table>
