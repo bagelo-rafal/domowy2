2d60f0bcd6f1054ee3afd1e2ae5744c50f6a8b62
Rafal <rafal@MacBook-Pro-Rafal.local>
2020-10-24 09:23:10 +0200

    Mail o niepowodzeniu płatności

---
+++ resources/views/mail/payment-sub-failed.blade.php
@@ -0,0 +1,162 @@
+
+
+<!-- [if (mso 16)]>
+   <style type="text/css">
+   a {text-decoration: none;}
+   </style>
+   <![endif]--><!-- [if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--><!-- [if gte mso 9]>
+   <xml>
+       <o:OfficeDocumentSettings>
+       <o:AllowPNG></o:AllowPNG>
+       <o:PixelsPerInch>96</o:PixelsPerInch>
+       </o:OfficeDocumentSettings>
+   </xml>
+   <![endif]-->
+<div class="es-wrapper-color" style="background-color: #f6f6f6;">
+   <!-- [if gte mso 9]>
+      <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+      	<v:fill type="tile" color="#f6f6f6"></v:fill>
+      </v:background>
+      <![endif]-->
+   <table class="es-wrapper" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px; padding: 0; margin: 0; width: 100%; height: 100%; background-repeat: repeat; background-position: center top;" width="100%" cellspacing="0" cellpadding="0">
+      <tbody>
+         <tr style="border-collapse: collapse;">
+            <td style="padding: 0; margin: 0;" valign="top">
+               <table class="es-content" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px; table-layout: fixed !important; width: 100%;" cellspacing="0" cellpadding="0" align="center">
+                  <tbody>
+                     <tr style="border-collapse: collapse;">
+                        <td style="padding: 0; margin: 0;" align="center">
+                           <table class="es-content-body" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px; background-color: #ffffff; width: 600px;" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff">
+                              <tbody>
+                                 <tr style="border-collapse: collapse;">
+                                    <td style="padding: 20px; margin: 0; background-color: transparent;" align="left" bgcolor="transparent">
+                                       <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" width="100%" cellspacing="0" cellpadding="0">
+                                          <tbody>
+                                             <tr style="border-collapse: collapse;">
+                                                <td style="padding: 0; margin: 0; width: 560px;" align="left">
+                                                   <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" role="presentation" width="100%" cellspacing="0" cellpadding="0">
+                                                      <tbody>
+                                                         <tr style="border-collapse: collapse;">
+                                                            <td class="es-infoblock es-m-txt-c" style="padding: 0; margin: 0; line-height: 0px; font-size: 0px; color: #cccccc;" align="left"><img style="display: block; border: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;" src="https://domowypediatra.pl/wp-content/uploads/2020/10/logo-och222.png" alt="" width="250" /></td>
+                                                         </tr>
+                                                      </tbody>
+                                                   </table>
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
+               <table class="es-content" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px; table-layout: fixed !important; width: 100%;" cellspacing="0" cellpadding="0" align="center">
+                  <tbody>
+                     <tr style="border-collapse: collapse;">
+                        <td style="padding: 0; margin: 0;" align="center">
+                           <table class="es-content-body" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px; background-color: #ffffff; width: 600px;" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff">
+                              <tbody>
+                                 <tr style="border-collapse: collapse;">
+                                    <td style="padding: 0; margin: 0; padding-top: 5px; padding-left: 20px; padding-right: 20px;" align="left">
+                                       <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" width="100%" cellspacing="0" cellpadding="0">
+                                          <tbody>
+                                             <tr style="border-collapse: collapse;">
+                                                <td style="padding: 0; margin: 0; width: 560px;" align="center" valign="top">
+                                                   <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" role="presentation" width="100%" cellspacing="0" cellpadding="0">
+                                                      <tbody>
+                                                         <tr style="border-collapse: collapse;">
+                                                            <td style="padding: 0; margin: 0;" align="left">
+                                                               <h1 style="margin: 0; line-height: 36px; mso-line-height-rule: exactly; font-family: arial, 'helvetica neue', helvetica, sans-serif; font-size: 30px; font-style: normal; font-weight: normal; color: #333333;">Rezerwacja e-wizyty</h1>
+                                                            </td>
+                                                         </tr>
+                                                      </tbody>
+                                                   </table>
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                                 <tr style="border-collapse: collapse;">
+                                    <td style="padding: 20px; margin: 0;" align="left">
+                                       <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" width="100%" cellspacing="0" cellpadding="0">
+                                          <tbody>
+                                             <tr style="border-collapse: collapse;">
+                                                <td style="padding: 0; margin: 0; width: 560px;" align="center" valign="top">
+                                                   <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" role="presentation" width="100%" cellspacing="0" cellpadding="0">
+                                                      <tbody>
+                                                         <tr style="border-collapse: collapse;">
+                                                            <td style="padding: 0; margin: 0;">Dostęp do szybkiego wsparcia medycznego został <strong>WSTRZYMANY.<br /></strong><br />Nie mogliśmy pobrać płatności. <span style="color: #3366ff;">ZAKTUALIZUJ METODĘ PŁATNOŚCI.</span><br /><br />DomowyPediatra.pl - NAZWA USŁUGI - OKRES PŁATNICZY - Przedłużenie automatyczne<br /><br />Data następnego rozliczenia: 23 Październik 2020<br /><br />Płatnik :<br /><br />Imię i nazwisko płatnia<br />Numer karty zagwiazdkowany opr&oacute;cz 4 ostatnich cyfr<br />Ulica i numer domu<br />Miejscowość i kod pocztowy<br /><br /><br />W przypadku pytań lub wątpliwości do Państwa dyspozycji pozostaje Dział Obsługi Klienta - <a href="mailto:kontakt@domowypediatra.pl" rel="noreferrer">kontakt@domowypediatra.pl</a><br /><br />Z wyrazami szacunku,<br />DomowyPediatra</td>
+                                                         </tr>
+                                                      </tbody>
+                                                   </table>
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                                 <tr style="border-collapse: collapse;">
+                                    <td style="margin: 0; padding: 10px 20px 20px 20px;" align="left">
+                                       <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" width="100%" cellspacing="0" cellpadding="0">
+                                          <tbody>
+                                             <tr style="border-collapse: collapse;">
+                                                <td style="padding: 0; margin: 0; width: 560px;" align="left">
+                                                   <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" role="presentation" width="100%" cellspacing="0" cellpadding="0">
+                                                      <tbody>
+                                                         <tr style="border-collapse: collapse;">
+                                                            <td style="padding: 0; margin: 0; font-size: 0;" align="left">
+                                                               <table class="es-table-not-adapt es-social" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" role="presentation" cellspacing="0" cellpadding="0">
+                                                                  <tbody>
+                                                                     <tr style="border-collapse: collapse;">
+                                                                        <td style="padding: 0; margin: 0; padding-right: 10px;" align="center" valign="top"><a style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-family: arial, 'helvetica neue', helvetica, sans-serif; font-size: 14px; text-decoration: underline; color: #2cb543;" href="https://www.facebook.com/DomowyPediatra" target="_blank" rel="noopener"><img style="display: block; border: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;" title="Facebook" src="https://inxbge.stripocdn.email/content/assets/img/social-icons/circle-colored/facebook-circle-colored.png" alt="Fb" width="32" /></a></td>
+                                                                        <td style="padding: 0; margin: 0; padding-right: 10px;" align="center" valign="top"><a style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-family: arial, 'helvetica neue', helvetica, sans-serif; font-size: 14px; text-decoration: underline; color: #2cb543;" href="https://www.instagram.com/DomowyPediatra/" target="_blank" rel="noopener"><img style="display: block; border: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;" title="Instagram" src="https://inxbge.stripocdn.email/content/assets/img/social-icons/circle-colored/instagram-circle-colored.png" alt="Inst" width="32" /></a></td>
+                                                                        <td style="padding: 0; margin: 0;" align="center" valign="top"><a style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-family: arial, 'helvetica neue', helvetica, sans-serif; font-size: 14px; text-decoration: underline; color: #2cb543;" href="https://domowypediatra.pl/" target="_blank" rel="noopener"><img style="display: block; border: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic;" title="Website" src="https://inxbge.stripocdn.email/content/assets/img/other-icons/circle-colored/link-circle-colored.png" alt="Website" width="32" /></a></td>
+                                                                     </tr>
+                                                                  </tbody>
+                                                               </table>
+                                                            </td>
+                                                         </tr>
+                                                      </tbody>
+                                                   </table>
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                                 <tr style="border-collapse: collapse;">
+                                    <td style="padding: 20px; margin: 0; background-color: #efefef;" align="left" bgcolor="#efefef">
+                                       <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" width="100%" cellspacing="0" cellpadding="0">
+                                          <tbody>
+                                             <tr style="border-collapse: collapse;">
+                                                <td style="padding: 0; margin: 0; width: 560px;" align="center" valign="top">
+                                                   <table style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; border-collapse: collapse; border-spacing: 0px;" role="presentation" width="100%" cellspacing="0" cellpadding="0">
+                                                      <tbody>
+                                                         <tr style="border-collapse: collapse;">
+                                                            <td style="padding: 0; margin: 0;">
+                                                               <p style="margin: 0; -webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-size: 11px; font-family: arial, 'helvetica neue', helvetica, sans-serif; line-height: 17px; color: #333333; text-align: justify;">Wiadomość wygenerowana automatycznie - prosimy nie odpowiadać. Administratorem Twoich danych osobowych jest MedPartner Sp. z o.o., ul. Graniczna 3, 62-030 Luboń, NIP: 7792469703. Potrzebujesz pomocy? Napisz do nas wiadomość na <strong><a style="-webkit-text-size-adjust: none; -ms-text-size-adjust: none; mso-line-height-rule: exactly; font-family: arial, 'helvetica neue', helvetica, sans-serif; font-size: 11px; text-decoration: underline; color: #2cb543; text-align: justify;" href="mailto:wizyta@domowypediatra.pl" target="_blank" rel="noopener">wizyta@domowypediatra.pl</a></strong></p>
+                                                            </td>
+                                                         </tr>
+                                                      </tbody>
+                                                   </table>
+                                                </td>
+                                             </tr>
+                                          </tbody>
+                                       </table>
+                                    </td>
+                                 </tr>
+                              </tbody>
+                           </table>
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
+            </td>
+         </tr>
+      </tbody>
+   </table>
+</div>
