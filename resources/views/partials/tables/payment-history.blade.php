04719394a9835c893e0cdc4eabbcdc7aa8bf7d6e
Rafal <rafal@MBP-Rafal.eastwest.com.pl>
2020-10-24 02:12:34 +0200

    Tabela z historią płatności użytkownika

---
+++ resources/views/partials/tables/payment-history.blade.php
@@ -0,0 +1,21 @@
+<table cellpadding="0" cellspacing="0" border="0" class="table table-bordered" id="paymentHistory" width="100%">
+				<thead>
+					<tr>
+						<th>Data wystawienia</th>
+						<th>Typ transakcji</th>
+						<th>Wartość</th>
+					</tr>
+				</thead>
+        <tbody>
+					<tr>
+	          <td>12/12/2020</td>
+	          <td>Subskrypcja</td>
+	          <td>19 zł</td>
+					</tr>
+					<tr>
+	          <td>12/12/2020</td>
+	          <td>E-wizyta</td>
+	          <td>69 zł</td>
+					</tr>
+        </tbody>
+			</table>
